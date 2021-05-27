b8a051f21a08ba69918f6b90d01f41831db5a524
David Basante <69875495+DavidBasante@users.noreply.github.com>
2021-05-27 09:12:30 -0300

    Add files via upload

---
+++ Aula- Variaveis.py
@@ -0,0 +1,13 @@
+name = 'David'
+age = 25
+height = 1.78
+great_than = age > 18
+weight = 89.0
+bmi = weight // (height ** 2)
+
+print('name: ', name)
+print('age: ', age)
+print('height: ', height)
+print('Great than: ', great_than)
+
+print(name, 'is', age, 'years old and his BMI is', bmi)
